ebe5a2503faa5959626a7cfc24b193c0c92c167c
Samra Arif <SArif@ad.lmkr.net>
2024-07-24 19:23:59 +0500

    Code refactoring

--- blog.py
+++ blog.py
@@ -1,48 +1,10 @@
-# Import necessary libraries
-import base64
-import datetime
-from io import BytesIO
+# Importing necessary libraries
+
 from flask import Flask, redirect, render_template, Response, request, url_for
-import plotly
-from welly import Well
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-import segfast
 import pandas as pd
-import dlisio
-import xarray as xr
-import json
-import os
-import threading
-from plotly import express as px
-from watchdog.observers import Observer
-import time
-from watchdog.events import FileSystemEventHandler
-import math
-import chart_studio.config as plotly_config
-import numpy as np
-import segyio
-import subprocess
-import pypyodbc
-import time
-import matplotlib
-#import subprocess
-import json
 from flask import Flask, render_template, request,jsonify
-from segysak.segy import segy_loader
-import segyio
 import numpy as np
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import re
-import pandas as pd
-import matplotlib.pyplot as plt
-import mpld3
-import plotly.express as px
 import mysql.connector
-import io
-import base64
 import time
 from datetime import datetime
 
@@ -60,15 +22,18 @@ def get_db_connection():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('Corr.html')
+    #main landing page would be Index page
+    return render_template('Index.html')
 
 @app.route('/login', methods=['POST'])
 def login():
+    #getting username and password which user entered
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
         try:
+            #creating db connection and matching credentials with authorized user saved in login table
             connection = get_db_connection()
             cursor = connection.cursor()
             query = "SELECT password FROM login WHERE username=%s"
@@ -79,25 +44,28 @@ def login():
             if row:
                 stored_password = row[0]
                 if stored_password == password:
+                    #if credentials are correct redirect to home page else show same page with error message
                     return render_template('home.html')
                 else:
-                    return render_template('Corr.html',output='Incorrect password')
-                    print('Incorrect password')
+                    return render_template('Index.html',output='Incorrect password')
+                    
             else:
-                return render_template('Corr.html',output='Username not found')
-                print('Username not found')
+                return render_template('Index.html',output='Username not found')
+                
 
         except : # type: ignore
-            return render_template('Corr.html',output='Could not connect to database')
+            return render_template('Index.html',output='Could not connect to database')
 
-    return render_template('Corr.html')
+    return render_template('Index.html')
 
 @app.route('/home')
 def home():
+    #returning home page
     return render_template('home.html')
 
 @app.route('/posts')
 def list_posts():
+    #if user selects view posts, then get all saved posts from database and view them
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
     query = "SELECT * FROM posts"
@@ -108,25 +76,13 @@ def list_posts():
 
 @app.route('/post/new', methods=['GET', 'POST'])
 def new_post():
-    # if request.method == 'POST':
-    #     title = request.form['title']
-    #     content = request.form['content']
-    #     author = request.form['author']
-    #     publication_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-    #     connection = get_db_connection()
-    #     cursor = connection.cursor()
-    #     query = "INSERT INTO posts (title, content, author, publication_date) VALUES (%s, %s, %s, %s)"
-    #     cursor.execute(query, (title, content, author, publication_date))
-    #     connection.commit()
-    #     connection.close()
-
-        # return redirect(url_for('list_posts'))
+    #if user selects create new post, redirect to new_post page where user could create posts
 
     return render_template('new_post.html')
 
 @app.route('/post/addnewpost', methods=['GET', 'POST'])
 def addnewpost():
+    #getting entered data from user and saving in database 
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
@@ -145,6 +101,7 @@ def addnewpost():
 
 @app.route('/post/edit/<int:id>', methods=['GET', 'POST'])
 def edit_post(id):
+    #get post which user edited and update in database
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
     if request.method == 'POST':
@@ -168,6 +125,7 @@ def edit_post(id):
 
 @app.route('/post/delete/<int:id>', methods=['POST'])
 def delete_post(id):
+    #delete the post which user selected
     connection = get_db_connection()
     cursor = connection.cursor()
     query = "DELETE FROM posts WHERE id=%s"
@@ -179,6 +137,7 @@ def delete_post(id):
 
 @app.route('/post/like/<int:id>', methods=['POST'])
 def like_post(id):
+    #update likes on each posts
     connection = get_db_connection()
     cursor = connection.cursor()
     query = "UPDATE posts SET likes = likes + 1 WHERE id=%s"
